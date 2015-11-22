eaf0ff655461931c0184975a9fccb561b56a67a2
ntabgoba <ntabgoba@gmail.com>
2015-11-22 13:29:32 +0900

    adding code for chachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##Code for one function that caches and another that computes inverse of a matrix
 
+## A function that creates a matrix and chache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(x){
+		mtrx <<- x;
+		inverse <<- NULL
+	}
+get <- function() return(mtrx);
+setinv <- function(inv) inverse <<- inv;
+getinv <- function() return(inverse);
+return(list(set = set, get=get, setinv = setinv, getinv = getinv))
 }
 
 
 ## Write a short comment describing this function
 
+## A function that computes inverse of the above matrix
+## If inverse has already been calculated and chaced by "makeCacheMatrix",cacheSolve
+## retrieves the cache inverse matrix instead of re computing it
+
 cacheSolve <- function(x, ...) {
+	inverse <- mtrx$getinv()
+	if(!is.null(inverse)) {
+		message("Getting chached data...")
+		return(inverse)
+	}
+	data <- mtrx$get()
+	inverse <- solve(data, ...)
+	mtrx$setinv(inverse)
+	return(inverse)
         ## Return a matrix that is the inverse of 'x'
 }
+
